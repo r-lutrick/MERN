4695546a675aa7df37ad58974a7bf4260d094cb5
Robert Lutrick <r.lutrick@icloud.com>
2023-01-12 13:45:53 -0600

    Updated functionality and render

--- prop_it_up/client/src/App.js
+++ prop_it_up/client/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './components/PropItUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <First firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
+      <First firstName="John" lastName="Smith" age={88} hairColor="Brown" />
+      <First firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown" />
+      <First firstName="Maria" lastName="Smith" age={62} hairColor="Brown" />
     </div>
   );
 }
