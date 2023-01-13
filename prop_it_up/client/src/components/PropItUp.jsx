7135e62c4138757be091f467b51f3d7e22d50b9f
Robert Lutrick <r.lutrick@icloud.com>
2023-01-12 19:48:37 -0600

    updated for functional component

--- prop_it_up/client/src/components/PropItUp.jsx
+++ prop_it_up/client/src/components/PropItUp.jsx
@@ -1,20 +1,35 @@
-import { render } from "@testing-library/react";
-import React, { Component } from "react";
+import React/*, { Component }*/ from "react";
+// import { render } from "@testing-library/react";
 
-class First extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        const { firstName, lastName, age, hairColor } = this.props
-        return (
-            <div>
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
-            </div >
-        )
-    }
+// Class Component
+// class First extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
+//     render() {
+//         const { firstName, lastName, age, hairColor } = this.props
+//         return (
+//             <div>
+//                 <h1>{lastName}, {firstName}</h1>
+//                 <p>Age: {age}</p>
+//                 <p>Hair Color: {hairColor}</p>
+//             </div >
+//         )
+//     }
+// }
+
+// Functional Component
+const First = (props) => { // (props) can also be written without the "()"
+    // No constructor needed
+    // No render() function needed
+    const { firstName, lastName, age, hairColor } = this.props
+    return (
+        <div>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </div>
+    )
 }
 
 export default First
